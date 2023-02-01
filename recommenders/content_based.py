e8a41edf93acec269b6ab45c3b2ecb9871b0d4e8
JasperZeroes <Jasperobed@gmail.com>
2023-02-01 13:14:10 +0100

    removed some heavy files

--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -35,9 +35,10 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Importing data
-movies = pd.read_csv('resources/data/movies.csv', sep = ',')
-ratings = pd.read_csv('resources/data/ratings.csv')
-movies.dropna(inplace=True)
+#movies = pd.read_csv('resources/data/movies_lit.csv', sep = ',')
+#ratings_df = pd.read_csv('resources/data/ratings_lit.csv')
+#ratings_df = ratings_df.drop(['timestamp'], axis=1,inplace=True)
+#movies.dropna(inplace=True)
 
 def data_preprocessing(subset_size):
     """Prepare data for use within Content filtering algorithm.
@@ -53,11 +54,16 @@ def data_preprocessing(subset_size):
         Subset of movies selected for content-based filtering.
 
     """
-    # Split genre data into individual words.
-    movies['keyWords'] = movies['genres'].str.replace('|', ' ')
     # Subset of the data
-    movies_subset = movies[:subset_size]
+    movies_subset = movies.loc[:subset_size]
+    
+    # Split genre data into individual words.
+    movies_subset["keyWords"] = movies_subset["genres"].str.replace("|", " ")
+    movies_subset["keyWords"] = movies_subset["keyWords"].str.lower()
+    
     return movies_subset
+
+
 
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
 # You are, however, encouraged to change its content.  
@@ -78,35 +84,33 @@ def content_model(movie_list,top_n=10):
         Titles of the top-n movie recommendations to the user.
 
     """
-    # Initializing the empty list of recommended movies
-    recommended_movies = []
+    # process a subset of the dataframe
     data = data_preprocessing(27000)
+    
     # Instantiating and generating the count matrix
     count_vec = CountVectorizer()
     count_matrix = count_vec.fit_transform(data['keyWords'])
-    indices = pd.Series(data['title'])
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
-    # Getting the index of the movie that matches the title
-    idx_1 = indices[indices == movie_list[0]].index[0]
-    idx_2 = indices[indices == movie_list[1]].index[0]
-    idx_3 = indices[indices == movie_list[2]].index[0]
+    indices = pd.Series(data['title'])
+    
+    # Getting the index of the movies that match the title
+    idx_list = [indices[indices == movie].index[0] for movie in movie_list]
+    
     # Creating a Series with the similarity scores in descending order
-    rank_1 = cosine_sim[idx_1]
-    rank_2 = cosine_sim[idx_2]
-    rank_3 = cosine_sim[idx_3]
-    # Calculating the scores
-    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-    # Getting the indexes of the 10 most similar movies
-    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
+    score_series_list = [pd.Series(cosine_sim[idx]).sort_values(ascending = False) for idx in idx_list]
+    
+    # Appending the names of movies
+    listings = pd.concat(score_series_list).sort_values(ascending = False)
 
-    # Store movie names
+    # Create empty list to store movie names
     recommended_movies = []
+    
     # Appending the names of movies
     top_50_indexes = list(listings.iloc[1:50].index)
+
     # Removing chosen movies
-    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
+    top_indexes = np.setdiff1d(top_50_indexes,idx_list)
+
     for i in top_indexes[:top_n]:
-        recommended_movies.append(list(movies['title'])[i])
+        recommended_movies.append(list(data['title'])[i])
     return recommended_movies
